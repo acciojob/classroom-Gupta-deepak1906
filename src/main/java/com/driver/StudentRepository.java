0ba1e19c98e10c0ed298503779e767c0daea97f1
gdeep <gdeepakg9997@gmail.com>
2024-04-04 23:06:43 +0530

    ok code

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -19,40 +19,81 @@ public class StudentRepository {
 
     public void saveStudent(Student student){
         // your code goes here
+        studentMap.put(student.getName(),student);
     }
 
     public void saveTeacher(Teacher teacher){
         // your code goes here
+        teacherMap.put(teacher.getName(),teacher);
     }
 
     public void saveStudentTeacherPair(String student, String teacher){
         if(studentMap.containsKey(student) && teacherMap.containsKey(teacher)){
             // your code goes here
+            if(teacherStudentMapping.containsKey(teacher)){
+                List<String>studentList = teacherStudentMapping.get(teacher);
+                studentList.add(student);
+                teacherStudentMapping.put(teacher,studentList);
+            }
+            else{
+                List<String>stringList = new ArrayList<>();
+                stringList.add(student);
+                teacherStudentMapping.put(teacher,stringList);
+            }
+            Teacher teacher1  = teacherMap.get(teacher);
+            teacher1.setNumberOfStudents(teacher1.getNumberOfStudents()+1);
         }
     }
 
     public Student findStudent(String student){
         // your code goes here
+        if(studentMap.containsKey(student)){
+            return studentMap.get(student);
+        }
+        return null;
     }
 
     public Teacher findTeacher(String teacher){
         // your code goes here
+        if(teacherMap.containsKey(teacher)){
+            return teacherMap.get(teacher);
+        }
+        return null;
     }
 
     public List<String> findStudentsFromTeacher(String teacher){
         // your code goes here
         // find student list corresponding to a teacher
+        List<String>studentList = new ArrayList<>();
+        if(teacherStudentMapping.containsKey(teacher)){
+            studentList.addAll(teacherStudentMapping.get(teacher));
+        }
+        return studentList;
     }
 
     public List<String> findAllStudents(){
         // your code goes here
+        List<String>studentList = new ArrayList<>();
+        for(var e:studentMap.entrySet()){
+            studentList.add(e.getKey());
+        }
+        return  studentList;
     }
 
     public void deleteTeacher(String teacher){
         // your code goes here
+        for(String name :teacherStudentMapping.get(teacher)){
+            studentMap.remove(name);
+        }
+        teacherMap.remove(teacher);
+        teacherStudentMapping.remove(teacher);
     }
 
     public void deleteAllTeachers(){
         // your code goes here
+        for(String name:teacherMap.keySet()){
+            deleteTeacher(name);
+            teacherMap.remove(name);
+        }
     }
 }
--- src/main/java/com/driver/StudentService.java
+++ src/main/java/com/driver/StudentService.java
@@ -11,39 +11,51 @@ public class StudentService {
     @Autowired
     StudentRepository studentRepository;
 
-    public void addStudent(Student student){
+    public void  addStudent(Student student){
+
         studentRepository.saveStudent(student);
+       // return "New student added succesfully";
     }
 
     public void addTeacher(Teacher teacher){
+
         studentRepository.saveTeacher(teacher);
+        //return "New teacher added successfully";
     }
 
     public void createStudentTeacherPair(String student, String teacher){
         studentRepository.saveStudentTeacherPair(student, teacher);
+       // return "New student-teacher pair added successfully";
     }
 
     public Student findStudent(String studentName){
+      //  Student student =studentRepository.findStudent(studentName);
         return studentRepository.findStudent(studentName);
     }
 
     public Teacher findTeacher(String teacherName){
+       // Teacher teacher = studentRepository.findTeacher(teacherName);
         return studentRepository.findTeacher(teacherName);
     }
 
     public List<String> findStudentsFromTeacher(String teacher){
+       // List<String>list=studentRepository.findStudentsFromTeacher()
         return studentRepository.findStudentsFromTeacher(teacher);
     }
 
     public List<String> findAllStudents(){
+      //  List<String> list=studentRepository.findAllStudents();
         return studentRepository.findAllStudents();
+
     }
 
     public void deleteTeacher(String teacher){
+
         studentRepository.deleteTeacher(teacher);
     }
 
     public void deleteAllTeachers(){
+
         studentRepository.deleteAllTeachers();
     }
 }
